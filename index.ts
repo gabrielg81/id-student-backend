4c93cf30bc32be1159547c8ba12e5b1c2709884f
Gabriel Silva da Cruz <gabrielscruz.g81@gmail.com>
2022-07-24 02:53:34 -0300

    fix for deploy azure

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
-app.listen(() => console.log("Server ON!"));
 
-/* const port = 80
+/*app.listen(() => console.log("Server ON!"));*/
+
+const port = 8080;
 app.listen(port, () => console.log(`Server ON! Port: ${port}`));
- */
